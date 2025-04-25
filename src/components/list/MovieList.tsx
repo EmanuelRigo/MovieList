8003d9d3c8f3c06ffc280b58be044a2c6ac77b78
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-04-25 19:39:41 -0300

    commit

--- src/components/list/MovieList.tsx
+++ src/components/list/MovieList.tsx
@@ -6,8 +6,12 @@ const data = res.response.movies;
 
 export default function MovieList() {
   return (
-    <div className="flex flex-grow-1  h-full ">
-      <MovieListClient list={data} />
+    <div className="flex flex-grow-1 h-full">
+      {data && data.length > 0 ? (
+        <MovieListClient list={data} />
+      ) : (
+        <div className="text-gray-400 m-auto text-center">No movies found.</div>
+      )}
     </div>
   );
 }
