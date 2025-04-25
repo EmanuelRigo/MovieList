863cf4760a90bb280e6f99c72cfaed285291fe1d
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-04-25 19:57:46 -0300

    commit]

--- src/components/list/MovieList.tsx
+++ src/components/list/MovieList.tsx
@@ -1,25 +1,22 @@
 import MovieListClient from "./MovieListClient";
 import { getUserMovies } from "../widgets/movies.api";
-import { MovieDB } from "@/context/interfaces/movieTypes";
+
+async function loadData() {
+  const res = await getUserMovies();
+  const data = res.response.movies;
+  console.log("🚀 ~ loadData ~ data:", data);
+  return data;
+}
 
 export default async function MovieList() {
-  let data: MovieDB[] = [];
-
-  try {
-    const res = await getUserMovies();
-    data = res?.response?.movies ?? [];
-  } catch (error) {
-    console.error("Error loading movies:", error);
-  }
+  const data = await loadData();
 
   return (
     <div className="flex flex-grow-1 h-full">
-      {data.length > 0 ? (
+      {data && data.length > 0 ? (
         <MovieListClient list={data} />
       ) : (
-        <div className="text-gray-400 m-auto text-center">
-          No movies found or failed to load.
-        </div>
+        <div className="text-gray-400 m-auto text-center">No movies found.</div>
       )}
     </div>
   );
